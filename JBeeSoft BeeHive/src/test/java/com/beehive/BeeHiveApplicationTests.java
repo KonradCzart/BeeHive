b7459a61328eaefa992e867e64849694f8b3d67e
MatCoder77 <matkimzag@wp.pl>
2018-11-18 20:10:08 +0100

    Added tests

---
+++ JBeeSoft BeeHive/src/test/java/com/beehive/BeeHiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beehive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeeHiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
