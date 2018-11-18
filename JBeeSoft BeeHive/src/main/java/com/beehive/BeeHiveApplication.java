544b131c2c5ab1a60cdc5c3a78559322b7f461c3
konrad <konradczart@interia.pl>
2018-11-18 20:03:59 +0100

    Start server message

---
+++ JBeeSoft BeeHive/src/main/java/com/beehive/BeeHiveApplication.java
@@ -0,0 +1,26 @@
+package com.beehive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		BeeHiveApplication.class,
+		Jsr310JpaConverters.class
+})
+public class BeeHiveApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeHiveApplication.class, args);
+	}
+}
