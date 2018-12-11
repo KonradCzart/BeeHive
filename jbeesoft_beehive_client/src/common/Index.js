60cc2f843640cd5d6e464b7bd771ed195916cfbb
filipbk <filipp973@gmail.com>
2018-12-11 13:14:21 +0100

    Added names "BeeHive"

---
+++ jbeesoft_beehive_client/src/common/Index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Index.css';
+
+class Index extends Component {
+	render() {
+		return (
+			<div className="index-welcome">
+				<h1 className="title">
+					Welcome to BeeHive - absolutely the best place for your bees!
+				</h1>
+			</div>
+		);
+	}
+}
+
+export default Index;
