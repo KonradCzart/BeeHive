b84350070dfa749906d09bd4b13e5bad0c8405b2
konrad <konradczart@interia.pl>
2018-12-12 17:43:32 +0100

    new apiary endpoint

--- JBeeSoft BeeHive/src/main/java/com/beehive/domain/apiary/ApiaryService.java
+++ JBeeSoft BeeHive/src/main/java/com/beehive/domain/apiary/ApiaryService.java
@@ -1,8 +1,10 @@
 package com.beehive.domain.apiary;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,9 @@ import com.beehive.domain.location.LocationRepository;
 import com.beehive.domain.location.LocationService;
 import com.beehive.domain.user.User;
 import com.beehive.domain.user.UserRepository;
+import com.beehive.infrastructure.payload.ApiaryDTO;
 import com.beehive.infrastructure.payload.ApiaryRequest;
+import com.beehive.infrastructure.security.UserPrincipal;
 
 @Service
 public class ApiaryService {
@@ -41,6 +45,21 @@ public class ApiaryService {
 		Apiary apiary = new Apiary(apiaryRequest.getName(), apiaryOwner, apiaryLocation);
 		
 		return apiaryRepository.save(apiary);
+	}
+	
+	public List<ApiaryDTO> getCurrentUserApiary(UserPrincipal currentUser){
+		
+		List<Apiary> apiaries= apiaryRepository.findByOwnerId(currentUser.getId());		
+		
+		return apiaries.stream().map(apiary -> mapApiatyToApiaryDTO(apiary)).collect(Collectors.toList());
+	}
+	
+	public ApiaryDTO mapApiatyToApiaryDTO(Apiary apiary) {
+		return ApiaryDTO.builder()
+				.withId(apiary.getId())
+				.withName(apiary.getName())
+				.withLocation(apiary.getLocation())
+				.build();
 	}
 	
 }
