689d5b99f0f4420be89699837dee1a3fd95800d5
Wera <woziemniak@gmail.com>
2018-12-16 21:15:34 +0100

    Add working WeatherAPI

---
+++ Weather/src/main/java/record.java
@@ -0,0 +1,19 @@
+public class record {
+    public double rainMililitersPer3h;
+
+    public double minTemp;
+    public double maxTemp;
+    public double temp;
+    public double pressure;
+    public int humidity;
+
+    public int cloudsPercentage;
+
+    public double windSpeed;
+    public double windDeg;
+
+    public String weatherType;
+
+
+}
+
