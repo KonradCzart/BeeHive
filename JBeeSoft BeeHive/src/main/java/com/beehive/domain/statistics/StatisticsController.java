3cdf5ddcb53381e9383b2a6ebe45174f7b363e6f
konrad <konradczart@interia.pl>
2019-01-19 20:49:43 +0100

    repair bug with get method

--- JBeeSoft BeeHive/src/main/java/com/beehive/domain/statistics/StatisticsController.java
+++ JBeeSoft BeeHive/src/main/java/com/beehive/domain/statistics/StatisticsController.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +47,7 @@ public class StatisticsController {
 	@Autowired
 	private PrivilegeService privilegeService;
 	
-	@GetMapping("/feeding/{apiaryId}")
+	@PostMapping("/feeding/{apiaryId}")
 	@PreAuthorize("hasRole('USER')")
 	public List<StatisticsDTO> feedingStatistics(@CurrentUser UserPrincipal currentUser, @RequestBody StatisticsRequest statisticsRequest, @PathVariable Long apiaryId){
 		
@@ -56,7 +56,7 @@ public class StatisticsController {
 		return statisticsService.getFeedingStatisticsForHives(hives, statisticsRequest.getBeginningDate(), statisticsRequest.getEndDate());
 	}
 	
-	@GetMapping("/treatment/{apiaryId}")
+	@PostMapping("/treatment/{apiaryId}")
 	@PreAuthorize("hasRole('USER')")
 	public List<StatisticsDTO> treatmentStatistics(@CurrentUser UserPrincipal currentUser, @RequestBody StatisticsRequest statisticsRequest, @PathVariable Long apiaryId){
 		
@@ -65,7 +65,7 @@ public class StatisticsController {
 		return statisticsService.getTreatmentStatisticsForHives(hives, statisticsRequest.getBeginningDate(), statisticsRequest.getEndDate());
 	}
 	
-	@GetMapping("/honey/{apiaryId}")
+	@PostMapping("/honey/{apiaryId}")
 	@PreAuthorize("hasRole('USER')")
 	public List<StatisticsDTO> honeyStatistics(@CurrentUser UserPrincipal currentUser, @RequestBody StatisticsRequest statisticsRequest, @PathVariable Long apiaryId){
 		
@@ -74,7 +74,7 @@ public class StatisticsController {
 		return statisticsService.getHoneyStatisticsForHives(hives, statisticsRequest.getBeginningDate(), statisticsRequest.getEndDate());
 	}
 	
-	@GetMapping("/contributor/{apiaryId}")
+	@PostMapping("/contributor/{apiaryId}")
 	@PreAuthorize("hasRole('USER')")
 	public List<StatisticsContributorDTO> contributorStatistics(@CurrentUser UserPrincipal currentUser, @RequestBody StatisticsApiaryRequest statisticsRequest, @PathVariable Long apiaryId){
 		
